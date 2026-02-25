8c18b00cdefd88f520f1aafc5407c5547f40a992
Adam <aqws6361@gmail.com>
2026-02-25 10:41:26 +0800

    查詢參數 & JSON格式回應

--- src/main/java/training/web/Hello.java
+++ src/main/java/training/web/Hello.java
@@ -1,9 +1,12 @@
 package training.web;
+import java.util.List;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 @RestController
@@ -17,11 +20,26 @@ public class Hello {
     public String index() {
         return "Hello Spring Boot!";
     }
+
     // 處理來自路徑 /test 的請求
     @GetMapping("/test")
-    public String test() {
-        return "Hello Routing Test";
+    public Map test() {
+        int[] grades=new int[]{80, 75, 20, 100};
+        return Map.of("name", "Student Grades", "data", grades);
     }
+
+
+    // 處理來自路徑 /echo?name=名字 的請求
+    @GetMapping("/echo")
+    public String echo(@RequestParam String name) {
+        return "Hello " + name;
+    }
+    // 處理來自路徑 /add?n1=整數&n2=整數 的請求
+    @GetMapping("/add")
+    public Map add(@RequestParam int n1, @RequestParam int n2) {
+        return Map.of("ans",  n1+n2);
+    }
+
     // 處理來自路徑 /user/任意文字 的請求
     @GetMapping("/user/{name}")
     public String user(@PathVariable String name) {
